6c404e4809833acd43e39741f12423f4c8567ec6
Cine-Pixel <ikaumphriani@gmail.com>
2022-11-11 05:27:48 -0800

    feat(validation): add model validations

--- Midterm/Models/Todo.cs
+++ Midterm/Models/Todo.cs
@@ -17,13 +17,14 @@ namespace Midterm.Models
 
         public int Id { get; set; }
 
-        [StringLength(100)]
+        [StringLength(100, ErrorMessage = "Title cannot be longer that 100 characters")]
+        [Required(ErrorMessage = "Todo Title is Mandatory")]
         public string Title { get; set; }
 
-        [StringLength(500)]
+        [StringLength(500, ErrorMessage = "Title cannot be longer that 500 characters")]
         public string Description { get; set; }
 
-        public bool? Completed { get; set; }
+        public bool Completed { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Note> Notes { get; set; }
