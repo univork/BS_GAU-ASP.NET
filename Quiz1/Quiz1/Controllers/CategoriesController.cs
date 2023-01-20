34ed3400605a5e784580677634003d0e280df4b8
Cine-Pixel <ikaumphriani@gmail.com>
2023-01-20 14:11:27 +0400

    quiz1

---
+++ Quiz1/Quiz1/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiz1.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Smartphones
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult GetCategory(int id,string name="phone")
+        {
+            ViewBag.Message = id;
+            ViewBag.name =name;
+            return View();
+        }
+    }
+}
---
+++ Quiz1/Quiz1/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiz1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Computers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetProduct(int id)
+        {
+            ViewBag.Message = id;
+            return View();
+        }
+    }
+}
