ba13adce9aee03ffaf3df11ccc93d7f4fc436182
Cine-Pixel <ikaumphriani@gmail.com>
2023-01-29 18:13:11 +0400

    webapi

---
+++ E-Commerce-Web/Webapi/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using PMEntity;
+using PMRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Webapi.Controllers
+{
+    public class BookController : ApiController
+    {
+        private BookRepository repo = new BookRepository();
+
+        public List<Book> Get()
+        {
+            List<Book> books = repo.GetAll();
+            return books;
+        }
+        public Book Get(int id)
+        {
+
+            return repo.Get(id);
+        }
+        
+        public bool Post(Book book)
+        {
+            return repo.Insert(book);
+        }
+
+        public bool Put(Book book, bool warning)
+        {
+            return repo.Update(book, warning);
+        }
+
+        public int Delete(int id)
+        {
+            return repo.Delete(id);
+        }
+    }
+}
---
+++ E-Commerce-Web/Webapi/Controllers/ClothController.cs
@@ -0,0 +1,41 @@
+﻿using PMEntity;
+using PMRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Webapi.Controllers
+{
+    public class ClothController : ApiController
+    {
+        private ClothRepository repo = new ClothRepository();
+
+        public List<Cloth> Get()
+        {
+            return repo.GetAll();
+        }
+        public Cloth Get(int id)
+        {
+
+            return repo.Get(id);
+        }
+        
+        public bool Post(Cloth cloth)
+        {
+            return repo.Insert(cloth);
+        }
+
+        public bool Put(Cloth cloth, bool warning)
+        {
+            return repo.Update(cloth, warning);
+        }
+
+        public int Delete(int id)
+        {
+            return repo.Delete(id);
+        }
+    }
+}
